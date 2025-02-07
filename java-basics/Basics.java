dcf8cdd91cfe4fc0c4638c06e795985ecf6461ea
OsafTheDeveloper <>
2025-02-07 22:02:47 +0500

    first commit

---
+++ java-basics/Basics.java
@@ -0,0 +1,24 @@
+
+public class Basics {
+    public static void main(String[] args) {
+        // Variable
+
+        int age = 20;
+        // System.out.println(age);
+
+        // IF-ELSE
+        int num = 10;
+        if (num == 0)
+
+        {
+            System.out.println("The number is zero");
+        } else if (num < 0) {
+            System.out.println("The number is negative");
+
+        } else if (num > 0) {
+
+            System.out.println("The number is positive");
+        }
+    }
+
+}
